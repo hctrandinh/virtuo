1313c5866a3212ff71dfeb96db0167dd527a062b
TRAN-DINH Huân Cyrille <huan@mbpdetrandinh.home>
2020-01-10 21:54:35 +0100

    Part2

--- public/index.js
+++ public/index.js
@@ -162,23 +162,8 @@ console.log(cars);
 console.log(rentals);
 console.log(actors);
 
-var prices = new Array();
-var carId = new Array();
-var testing = new Array();
-
-rentals.forEach(element => {
-  carId.push(element.carId);
-});
-
-console.log(carId);
-console.log(prices);
-
-carId.forEach(element => {
-  var test = (rentals).find((x => x.carId === element));
-  testing.push(test.pickupDate);
-});
-
-console.log(testing);
+//PART 1
+var pt1 = [] //Result part 1.
 
 function dayDiff(d1, d2)
 {
@@ -187,49 +172,66 @@ function dayDiff(d1, d2)
   return new Number(d2 - d1).toFixed(0);
 }
 
-carId.forEach(element => {
-  var value1 = cars.find(x => x.id === element);
-  prices.push(
-    value1.pricePerDay * (new Date(rentals.find(x => x.carId === element).pickupDate.split('-')[2],rentals.find(x => x.carId === element).pickupDate.split('-')[1],rentals.find(x => x.carId === element).pickupDate.split('-')[0]).getDate()
-    - new Date(rentals.find(x => x.carId === element).returnDate.split('-')[2],rentals.find(x => x.carId === element).returnDate.split('-')[1],rentals.find(x => x.carId === element).returnDate.split('-')[0]).getDate())
-    + value1.pricePerKm * rentals.find(x => x.carId === element).distance
-  )
+rentals.forEach(element => {
+  var rental = {};
+  rental.rentalid = element.carId;
+  rental.rentaldriver = element.driver;
+  var value1 = cars.find(x => x.id === element.carId);
+  var prix = value1.pricePerDay * (new Date(rentals.find(x => x.carId === element.carId).pickupDate.split('-')[2],rentals.find(x => x.carId === element.carId).pickupDate.split('-')[1],rentals.find(x => x.carId === element.carId).pickupDate.split('-')[0]).getDate()
+    - new Date(rentals.find(x => x.carId === element.carId).returnDate.split('-')[2],rentals.find(x => x.carId === element.carId).returnDate.split('-')[1],rentals.find(x => x.carId === element.carId).returnDate.split('-')[0]).getDate())
+    + value1.pricePerKm * rentals.find(x => x.carId === element.carId).distance;
+  rental.rentalpricing = prix;
+  pt1.push(rental);
 });
 
-console.log(prices);
+console.log(pt1);
 
+//PART2
 
-var part_1 = [{
-  'rentalid': '893a04a3-e447-41fe-beec-9a6bfff6fdb4',
-  'price': prices[0]
-},
-{
-  'rentalid': 'bc16add4-9b1d-416c-b6e8-2d5103cade80',
-  'price': prices[1]
-},
-{
-  'rentalid': '8c1789c0-8e6a-48e3-8ee5-a6d4da682f2a',
-  'price': prices[2]
-}
-];
+var pt2 = [] //Result part 1.
 
+rentals.forEach(element => {
+  var rental = {};
+  rental.rentalid = element.carId;
+  rental.rentaldriver = element.driver;
 
-console.log(part_1)
+  var price2 = 0;
+  var value1 = cars.find(x => x.id === element.carId);
+  var value2 = (new Date(rentals.find(x => x.carId === element.carId).pickupDate.split('-')[2],rentals.find(x => x.carId === element.carId).pickupDate.split('-')[1],rentals.find(x => x.carId === element.carId).pickupDate.split('-')[0]).getDate()
+  - new Date(rentals.find(x => x.carId === element.carId).returnDate.split('-')[2],rentals.find(x => x.carId === element.carId).returnDate.split('-')[1],rentals.find(x => x.carId === element.carId).returnDate.split('-')[0]).getDate());
+  if(value2 < 1)
+  {
+    price2 = value1.pricePerDay * value2 + value1.pricePerKm * rentals.find(x => x.carId === element.carId).distance;
+    rental.rentalpricing = price2;
+    pt2.push(rental);
+  }
+  if(value2 >= 1 && value2 < 4)
+  {
+    price2 = (value1.pricePerDay * value2 + value1.pricePerKm * rentals.find(x => x.carId === element.carId).distance) * 0.90;
+    rental.rentalpricing = price2;
+    pt2.push(rental);
+  }
+  if(value2 >= 4 && value2 < 10)
+  {
+    price2 = (value1.pricePerDay * value2 + value1.pricePerKm * rentals.find(x => x.carId === element.carId).distance) * 0.70;
+    rental.rentalpricing = price2;
+    pt2.push(rental);
+  }
+  if(value2 >= 10)
+  {
+    price2 = (value1.pricePerDay * value2 + value1.pricePerKm * rentals.find(x => x.carId === element.carId).distance) * 0.50;
+    rental.rentalpricing = price2;
+    pt2.push(rental);
+  }
+});
 
-
-
-
-
-
-
+console.log(pt2)
 
 
 function fetchPrice(id2) {
   var test = (cars).find(element => element.id==id2);
   return [test.pricePerDay,test.pricePerKm]
 }
-
-
 
 rentals.forEach(element => {
   var returnDate = new Date(element.returnDate);
